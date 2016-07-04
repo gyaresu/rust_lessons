001ba4d4e2808064e24bc002888321be4e5893c4
Gareth <me@gareth.codes>
2016-07-04 02:33:00 -0700

    mutability and variable bindings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    //let x = 5;
+    let (x, y) = (1, 2);
+    println!("x is: {}\ny is: {}", x, y);
+    let z: i32 = 342342342;
+    println!("z is: {}", z);
+    let mut a: i32 = 100;
+    println!("a is: {}", a);
+    a = 42;
+    println!("a is: {}", a);
+
 }
